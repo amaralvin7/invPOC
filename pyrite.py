955e749855444fb5faf909aa7ebeaac94d580844
amaralvin7 <amaralvin7@gmail.com>
2021-02-11 14:38:43 -0800

    Add PyritePlotter class, pickle_model() method for PyriteModel

--- pyrite.py
+++ pyrite.py
@@ -8,17 +8,21 @@ Created on Tue Feb  9 11:55:53 2021
 PYRITE Model (Particle cYcling Rates from Inversion of Tracers in the ocEan)
 """
 import time
-
 import numpy as np
 import pandas as pd
 import statsmodels.formula.api as smf
+import pickle
+import matplotlib.pyplot as plt
+import mpl_toolkits.axisartist as AA
+from mpl_toolkits.axes_grid1 import host_subplot
 #from varname import nameof
 
 class PyriteModel:
 
-    def __init__(self, gammas=[0.02]):
+    def __init__(self, gammas=[0.02], pickle_into='out/pyrite_Amaral21a.pkl'):
         
-        self.GAMMAS = gammas
+        self.gammas = gammas
+        self.pickled = pickle_into
         self.MIXED_LAYER_DEPTH = 30
         self.MAX_DEPTH = 500
         self.GRID_STEP = 5
@@ -34,9 +38,11 @@ class PyriteModel:
         self.unpack_tracers()
         self.define_params()
 
+        self.pickle_model()
+
     def __repr__(self):
 
-        return f'PyriteModel(gammas={self.GAMMAS})'
+        return f'PyriteModel(gammas={self.gammas})'
                     
     def load_data(self):
         
@@ -51,7 +57,7 @@ class PyriteModel:
     
     def define_params(self):
         
-        P30_PRIOR, P30_PRIOR_E, LP_PRIOR, LP_PRIOR_E = self.process_npp_data()
+        P30_prior, P30_prior_e, Lp_prior, Lp_prior_e = self.process_npp_data()
         
         self.ws = PyriteParam(2, 2, 'ws', '$w_S$')
         self.wl = PyriteParam(20, 15, 'wl', '$w_L$')
@@ -63,9 +69,9 @@ class PyriteModel:
                                'Bm2', '$\\beta_{-2}$')
         self.Bm1s = PyriteParam(0.1, 0.1, 'Bm1s', '$\\beta_{-1,S}$')
         self.Bm1l = PyriteParam(0.15, 0.15, 'Bm1l', '$\\beta_{-1,L}$')
-        self.P30 = PyriteParam(P30_PRIOR, P30_PRIOR_E, 'P30', '$\.P_{S,30}$',
+        self.P30 = PyriteParam(P30_prior, P30_prior_e, 'P30', '$\.P_{S,30}$',
                                depth_vary=False)
-        self.Lp = PyriteParam(LP_PRIOR, LP_PRIOR_E, 'Lp', '$L_P$',
+        self.Lp = PyriteParam(Lp_prior, Lp_prior_e, 'Lp', '$L_P$',
                               depth_vary=False)
 
         self.model_params = (self.ws, self.wl, self.B2p, self.Bm2, self.Bm1s,
@@ -73,47 +79,131 @@ class PyriteModel:
 
     def process_npp_data(self):
         
-        NPP_DATA_RAW = self.DATA['npp']
-        NPP_DATA_CLEAN = NPP_DATA_RAW.loc[(NPP_DATA_RAW['npp'] > 0)]
+        npp_data_raw = self.DATA['npp']
+        npp_data_clean = npp_data_raw.loc[(npp_data_raw['npp'] > 0)]
         
         MIXED_LAYER_UPPER_BOUND, MIXED_LAYER_LOWER_BOUND = 28, 35
         
-        NPP_MIXED_LAYER = NPP_DATA_CLEAN.loc[
-            (NPP_DATA_CLEAN['target_depth'] >= MIXED_LAYER_UPPER_BOUND) & 
-            (NPP_DATA_CLEAN['target_depth'] <= MIXED_LAYER_LOWER_BOUND)]
+        npp_mixed_layer = npp_data_clean.loc[
+            (npp_data_clean['target_depth'] >= MIXED_LAYER_UPPER_BOUND) &
+            (npp_data_clean['target_depth'] <= MIXED_LAYER_LOWER_BOUND)]
         
-        NPP_BELOW_MIXED_LAYER = NPP_DATA_CLEAN.loc[
-            NPP_DATA_CLEAN['target_depth'] >=  MIXED_LAYER_UPPER_BOUND]
+        npp_below_mixed_layer = npp_data_clean.loc[
+            npp_data_clean['target_depth'] >=  MIXED_LAYER_UPPER_BOUND]
         
-        P30_PRIOR = NPP_MIXED_LAYER['npp'].mean()/self.MOLAR_MASS_C
-        P30_PRIOR_E = NPP_MIXED_LAYER['npp'].sem()/self.MOLAR_MASS_C
-        
-        NPP_REGRESSION = smf.ols(
-            formula='np.log(npp/(P30_PRIOR*self.MOLAR_MASS_C)) ~ target_depth',
-            data=NPP_BELOW_MIXED_LAYER).fit()
+        P30_prior = npp_mixed_layer['npp'].mean()/self.MOLAR_MASS_C
+        P30_prior_e = npp_mixed_layer['npp'].sem()/self.MOLAR_MASS_C
 
-        LP_PRIOR = -1/NPP_REGRESSION.params[1]
-        LP_PRIOR_E = NPP_REGRESSION.bse[1]/NPP_REGRESSION.params[1]**2
+        npp_regression = smf.ols(
+            formula='np.log(npp/(P30_prior*self.MOLAR_MASS_C)) ~ target_depth',
+            data=npp_below_mixed_layer).fit()
+
+        Lp_prior = -1/npp_regression.params[1]
+        Lp_prior_e = npp_regression.bse[1]/npp_regression.params[1]**2
         
-        return P30_PRIOR, P30_PRIOR_E, LP_PRIOR, LP_PRIOR_E
+        return P30_prior, P30_prior_e, Lp_prior, Lp_prior_e
+
+    def pickle_model(self):
+
+        with open(self.pickled, 'wb') as file:
+            pickle.dump(self,file)
 
 class PyriteParam:
 
     def __init__(self, prior, prior_error, name, label, depth_vary=True):
         
-        self.PRIOR = prior
-        self.PRIOR_E = prior_error
-        self.NAME = name
-        self.LABEL = label
-        self.DV = depth_vary
+        self.prior = prior
+        self.prior_e = prior_error
+        self.name = name
+        self.label = label
+        self.dv = depth_vary
 
     def __repr__(self):
 
         return f'PyriteParam({self.NAME})'
 
+class PyritePlotter:
+
+    def __init__(self, pickled_model):
+
+        with open(pickled_model.pickled, 'rb') as file:
+            self.model = pickle.load(file)
+
+        self.define_colors()
+        self.plot_hydrography()
+
+    def define_colors(self):
+
+        self.BLACK = '#000000'
+        self.ORANGE = '#E69F00'
+        self.SKY = '#56B4E9'
+        self.GREEN = '#009E73'
+        self.YELLOW = '#F0E442'
+        self.BLUE = '#0072B2'
+        self.VERMILLION = '#D55E00'
+        self.RADISH = '#CC79A7'
+
+    def plot_hydrography(self):
+
+        hydro_df = self.model.DATA['hydrography']
+
+        fig = plt.figure()
+        host = host_subplot(111, axes_class=AA.Axes, figure=fig)
+        plt.subplots_adjust(top=0.75)
+        par1 = host.twiny()
+        par2 = host.twiny()
+
+        par1.axis['top'].toggle(all=True)
+        offset = 40
+        new_fixed_axis = par2.get_grid_helper().new_fixed_axis
+        par2.axis['top'] = new_fixed_axis(loc='top', axes=par2,
+                                          offset=(0, offset))
+        par2.axis['top'].toggle(all=True)
+
+        host.set_ylim(0, 520)
+        host.invert_yaxis(), host.grid(axis='y', alpha=0.5)
+        host.set_xlim(24, 27.4)
+        par1.set_xlim(3, 14.8)
+        par2.set_xlim(32, 34.5)
+
+        host.set_ylabel('Depth (m)',fontsize=14)
+        host.set_xlabel('$\sigma_T$ (kg m$^{-3}$)')
+        par1.set_xlabel('Temperature (°C)')
+        par2.set_xlabel('Salinity (PSU)')
+
+        host.plot(hydro_df['sigT_kgpmc'], hydro_df['depth'], c=self.ORANGE,
+                  marker='o')
+        par1.plot(hydro_df['t_c'], hydro_df['depth'], c=self.GREEN,
+                  marker='o')
+        par2.plot(hydro_df['s_psu'], hydro_df['depth'], c=self.BLUE,
+                  marker='o')
+        host.axhline(self.model.MIXED_LAYER_DEPTH, c=self.BLACK, ls=':',
+                     zorder=3)
+        host.axhline(self.model.BOUNDARY, c=self.BLACK, ls='--', zorder=3)
+
+        host.axis['bottom'].label.set_color(self.ORANGE)
+        par1.axis['top'].label.set_color(self.GREEN)
+        par2.axis['top'].label.set_color(self.BLUE)
+
+        host.axis['bottom','left'].label.set_fontsize(14)
+        par1.axis['top'].label.set_fontsize(14)
+        par2.axis['top'].label.set_fontsize(14)
+
+        host.axis['bottom','left'].major_ticklabels.set_fontsize(12)
+        par1.axis['top'].major_ticklabels.set_fontsize(12)
+        par2.axis['top'].major_ticklabels.set_fontsize(12)
+
+        host.axis['bottom','left'].major_ticks.set_ticksize(6)
+        par1.axis['top'].major_ticks.set_ticksize(6)
+        par2.axis['top'].major_ticks.set_ticksize(6)
+
+        plt.savefig('out/hydrography.pdf')
+        plt.close()
+
 if __name__ == '__main__':
 
     start_time = time.time()
     model = PyriteModel()
+    plotter = PyritePlotter(model)
 
     print(f'--- {(time.time() - start_time)/60} minutes ---')
