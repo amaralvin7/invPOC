2211c3b257b30d6109791dfe33b7580481cdb8ce
amaralvin7 <amaralvin7@gmail.com>
2023-11-29 15:09:03 -0800

    Delete unused exports files

--- results/exports/out_compare.py
+++
@@ -1,7 +0,0 @@
-"""Compare if two text files are identical."""
-import filecmp
-
-f1 = 'test.txt'  # reference file
-f2 = 'out.txt'  # new output file to be compared to reference
-
-print(filecmp.cmp(f1, f2, shallow=False))
